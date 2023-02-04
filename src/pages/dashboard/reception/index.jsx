f3ba4b7a69578fc7fb5c5a558c3b46666af9de30
DezyNation <dezynationindia@gmail.com>
2023-02-04 16:48:51 +0530

    folder structure fixed

---
+++ src/pages/dashboard/reception/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+import Layout from './layout'
+
+const Dashboard = () => {
+  return (
+    <>
+        <Layout pageName={'Student Dashboard'}>
+          
+        </Layout>
+    </>
+  )
+}
+
+export default Dashboard
