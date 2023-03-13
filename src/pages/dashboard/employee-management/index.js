f24f6ffa6921b6b477b3b8b6e2e3b5039262ae4d
dhannjayyy <d.dhannjayyy@gmail.com>
2023-03-13 20:29:53 +0530

    Added formik object in the Employee management Drawer

--- src/pages/dashboard/employee-management/index.js
+++ src/pages/dashboard/employee-management/index.js
@@ -1,14 +1,17 @@
 import Index from "@/Components/Employee Management/index/Index";
 import InchargeSidebar from "@/Components/Layout Components/Sidebar/InchargeSidebar";
+import { FilesProvider } from "@/Components/Student/Student Assignment/FilesProvider";
 import { Flex } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <Flex>
-        <InchargeSidebar />
-        <Index />
-      </Flex>
+      <FilesProvider>
+        <Flex>
+          <InchargeSidebar />
+          <Index />
+        </Flex>
+      </FilesProvider>
     </>
   );
 }
